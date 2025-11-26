3072bb05c174cb49eafefb5fff9afb190fbb610c
EthanBa123 <ethan.balas@gmail.com>
2025-11-26 12:58:38 +0200

    Listeners

--- app/src/main/java/com/katza/omerpotashnik/MainActivity.java
+++ app/src/main/java/com/katza/omerpotashnik/MainActivity.java
@@ -1,16 +1,30 @@
 package com.katza.omerpotashnik;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     Button bt;
+    ImageView iv;
+
+    Switch sw;
+
+    SeekBar seekBar;
+
+    boolean visable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,65 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        visable = true;
+
+        initViews();
+    }
+
+    private void initViews() {
+
+        iv = findViewById(R.id.iv);
+
+        sw = findViewById(R.id.sw);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    iv.setVisibility(View.INVISIBLE);
+                else
+                    iv.setVisibility(View.VISIBLE);
+            }
+        });
+
+        seekBar = findViewById(R.id.sb);
+        seekBar.setMin(0);          // Minimum value
+        seekBar.setMax(100);        // Maximum value
+        seekBar.setProgress(100);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                // Convert progress (0–100) to alpha (0f–1f)
+                float alphaValue = progress / 100f;
+                iv.setAlpha(alphaValue);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "onStartTrackingTouch", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "onStopTrackingTouch", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        bt = findViewById(R.id.btn1);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Btn1", Toast.LENGTH_SHORT).show();
+                if (visable)
+                    iv.setVisibility(View.INVISIBLE);
+                else
+                    iv.setVisibility(View.VISIBLE);
+                visable = !visable;
+            }
         });
     }
 
