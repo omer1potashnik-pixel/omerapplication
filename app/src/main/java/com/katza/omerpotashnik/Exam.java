432635d26025bb8745d3082cbeba89dcc99113a7
User <omer1.potashnik@gmail.com>
2026-05-20 15:01:20 +0300

    list arrey

---
+++ app/src/main/java/com/katza/omerpotashnik/Exam.java
@@ -0,0 +1,17 @@
+package com.katza.omerpotashnik;
+public class Exam {
+    private String name;
+    private int score;
+    private boolean isPassed;
+
+    public Exam(String name, int score) {
+        this.name = name;
+        this.score = score;
+        // נניח שציון עובר הוא 60 ומעלה
+        this.isPassed = score >= 60;
+    }
+
+    public String getName() { return name; }
+    public int getScore() { return score; }
+    public boolean isPassed() { return isPassed; }
+}
